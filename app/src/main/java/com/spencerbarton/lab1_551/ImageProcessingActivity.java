2e1b0efcc0c1cb323ce588b93443709a6cf16dd7
David Lu <davidelu93@gmail.com>
2015-01-26 20:24:29 -0500

    Add ImageProcessingActivity

--- app/src/main/java/com/spencerbarton/lab1_551/ImageProcessingActivity.java
+++ app/src/main/java/com/spencerbarton/lab1_551/ImageProcessingActivity.java
@@ -1,28 +1,26 @@
 package com.spencerbarton.lab1_551;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.Intent;
 
 
-public class MainActivity extends ActionBarActivity {
-
-    private static final String TAG = "MainActivity";
+public class ImageProcessingActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_image_processing);
+        Intent intent = getIntent();
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        getMenuInflater().inflate(R.menu.menu_image_processing, menu);
         return true;
     }
 
@@ -34,16 +32,10 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        switch (id) {
-            case R.id.action_audio:
-                Log.d(TAG, "Audio");
-                return true;
-            case R.id.action_image:
-                Log.d(TAG, "Image");
-                Intent intent = new Intent(this, ImageProcessingActivity.class);
-                startActivity(intent);
-                return true;
+        if (id == R.id.action_settings) {
+            return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
