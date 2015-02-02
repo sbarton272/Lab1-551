14388bdd717128843602bbf78acb31210ac94939
Spencer Barton <sbarton272@gmail.com>
2015-02-02 10:33:16 -0500

    Wrote main reading

--- filtering/main.c
+++ filtering/main.c
@@ -18,8 +18,24 @@
 
 int main(int argc, char *argv[]) {
 
+  audio_t* audio = malloc(sizeof(audio_t));
+  readDatFile(AUDIO_FLT_FILE, audio);
+  freeAudio(audio);
+  printf("Saved to %s\n", AUDIO_FLT_FILE);
+  
+  printf("Saved to %s\n", AUDIO_MOD_FILE);
+
+}
+
+/*==================================
+ * Private
+ *==================================*/
+
+// 32 bit float, little-endian
+void readDatFile(char* filename, audio_t* data) {
+
   // Read in and parse data
-  FILE* dataFp = fopen(AUDIO_IN_FILE, "r");
+  FILE* dataFp = fopen(filename, "r");
 
   // Error check
   if (dataFp == NULL) {
@@ -27,7 +43,20 @@ int main(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   }
 
-  printf("Saved to %s\n", AUDIO_FLT_FILE);
-  printf("Saved to %s\n", AUDIO_MOD_FILE);
+  // Allocate audio file and buffer space
+  audio->data = malloc(DATA_SIZE*sizeof(float));
 
+  // Read as floats
+  audio->len = fread((void *)audio->data, sizeof(float), DATA_SIZE, dataFp);
+
+  // Close file
+  fclose(dataFp);
 }
+
+void writeDatFile(char* filename, float[] data, uint32_t dataLen);
+
+void freeAudio(audio_t* audio) {
+  free(&audio.data);
+  free(audio);
+}
+
--- filtering/main.h
+++ filtering/main.h
@@ -22,13 +22,15 @@ const char *AUDIO_IN_FILE = "hw1_noisy.dat";
 const char *AUDIO_FLT_FILE = "hw1_filtered.dat";
 const char *AUDIO_MOD_FILE = "hw1_modulated.dat";
 
+#define DATA_SIZE (262144)
+
 /*==================================
  * Structs
  *==================================*/
 
 typedef struct
 {
-	float[] audio;
+	float[] data;
 	uint32_t len;
 } audio_t;
 
@@ -42,8 +44,10 @@ int main(int argc, char *argv[]);
  * Private
  *==================================*/
 
-static audio_t readDatFile(char* filename);
+static audio_t* readDatFile(char* filename);
 
 static void writeDatFile(char* filename, float[] data, uint32_t dataLen);
+
+static void freeAudio(audio_t* audio);
 
 #endif // _MAIN_H
